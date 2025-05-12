a3955d89932e77319d390a967d3b83833e526fde
David Keith <dave.keith@outlook.com>
2025-05-12 10:35:36 -0300

    Meeting update
    Little tweaks during our meeting.

--- Scripts/trophic_level_RUNME.R
+++ Scripts/trophic_level_RUNME.R
@@ -89,9 +89,10 @@ for(s in names(for.tune.all)) if(s %in% eco.stocks) eco.lambdas[[s]] <- res.lamb
 # ASR.long <- left_join(ASR.long,all.troph,by = join_by(Gen.Spec==Species))
 
 # Try the function
-exploit.mn <- data.frame(ex.mn = rep(0.1,length(stock.lst)),Stock = names(stock.lst))
-exploit.sd <- data.frame(ex.sd = rep(0.1,length(stock.lst)),Stock = names(stock.lst))
+exploit.mn <- data.frame(ex.mn = c(1,0,rep(0.1,length(stock.lst)-2)),Stock = names(stock.lst))
+exploit.sd <- data.frame(ex.sd = c(0,0,rep(0.1,length(stock.lst)-2)),Stock = names(stock.lst))
 
-test <- trophic.mod(stocks = stock.lst,lambdas= eco.lambdas,n.sims=100,er.mn = exploit.mn,er.sd = exploit.sd)
+test <- trophic.mod(stocks = stock.lst,lambdas= eco.lambdas,n.sims=n.sims,er.mn = exploit.mn,er.sd = exploit.sd,
+                    n.yrs.proj= n.yrs.proj)
 
 
